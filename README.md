beea78a51adf573748353e1ea1116b7e73048330
root <martin.weiss@suse.com>
2026-04-30 16:50:35 +0200

    working on SMLM and SSL

--- README.md
+++ README.md
@@ -29,35 +29,58 @@ We have to use kiwi 10.3 for debian - which is not available as container.
 When building on SLES - we have to use boxbuild as the kiwi image we can use via podman does not support debian.
 https://osinside.github.io/kiwi/plugins/self_contained.html
 -> this needs python3-kiwi_boxed_plugin
+Hint: boxbuild uses https://github.com/OSInside/kiwi-boxed-plugin/blob/main/kiwi_boxed_plugin/config/kiwi_boxed_plugin.yml
 
 This depends on python313-progressbar2
 -> get it from https://download.opensuse.org/repositories/devel:/languages:/python:/backports/16.0/
 
 Remove the repos from the kiwi xml as we need to add them depending on the stage we are building for..
 
-For repos we need to add
+For repos we need to add (or 13 in Builder instead of Staging)
 --add-repo obs://Virtualization:Appliances:Staging/Debian_12_update,apt-deb,kiwi,,,,,,,false \
 --add-repo obs://Virtualization:Appliances:Staging/Debian_12_x86_64,apt-deb,kiwi,,,,,,,false \
+
+--add-repo obs://Virtualization:Appliances:Builder/"Debian_"$DEBIAN_VER,apt-deb,kiwi,,,,,,,false \
+--add-repo obs://Virtualization:Appliances:Builder/"Debian_"$DEBIAN_VER"_x86_64",apt-deb,kiwi,,,,,,,false \
+
 for the iso build as we need a few packages for dracut
 or the corresponding versions.. see build*.sh
 
 To register against SMLM
+------------------------
+
 ssh into the box as mweiss
 su -
 hostnamectl hostname debian13-1.suse
 
+For auto-sign-in to SMLM set the autosign grains and run bootstrap with the right activation key:
+
+mkdir -p /etc/venv-salt-minion/minion.d/
+
 cat <<EOF > /etc/venv-salt-minion/minion.d/autosign-grains.conf
-# create the grain
 grains:
     autosign_key: 4fea9511-8b25-4f7a-9071-8eb06c07bad0
-
-# send the grain as part of auth request
 autosign_grains:
     - autosign_key
 EOF
-
 curl http://susemanager.suse/pub/bootstrap/bootstrap.sh -o bootstrap.sh
 ACTIVATION_KEYS=1-debian13-test bash bootstrap.sh
 
 
-Hint: boxbuild uses https://github.com/OSInside/kiwi-boxed-plugin/blob/main/kiwi_boxed_plugin/config/kiwi_boxed_plugin.yml
+On SUSE Multi-Linux Manager:
+----------------------------
+
+Enable autosign:
+
+mgrctl term
+echo "autosign_grains_dir: /etc/salt/autosign_grains" > /etc/salt/master.d/autosign_grains.conf
+mkdir -p /etc/salt/autosign_grains
+echo "4fea9511-8b25-4f7a-9071-8eb06c07bad0" > /etc/salt/autosign_grains/autosign_key
+systemctl restart salt-master
+exit
+
+ToDo / next steps:
+
+- get build done with frozen SMLM channels
+- get personalization added - maybe see if combustion can work
+- see how we can add the boxbuild feature to SMLM (idea is to detect the OS in the kiwi xml and then select the right box for the right build...)
--- build-disk.sh
+++ build-disk.sh
@@ -10,7 +10,7 @@ KIWI_BOXES="/data/isos/kiwi_boxes"
 KIWI_IMAGE="public.ecr.aws/b9k1j9y6/kiwi:latest" #test release 10.3 with boxbuild support
 
 #VARIANT="boxbuild" # requires local installed kiwi 10.3 + boxbuild plugin
-VARIANT="podman" #<- can not work as long as we do not have an image with 10.3 and boxbuild..
+VARIANT="podman" #<- can not work as long as we do not have an image with 10.3 and boxbuild.. but works with 10.3 test image with boxbuild support above. Hint: entrypoint added so kiwi-ng / kiwi needs to be removed
 
 #DEBIAN="bookworm"
 #DEBIAN_VER="12"
@@ -23,9 +23,8 @@ rm -rf $TARGET_DIR/image
 mkdir -p $TARGET_DIR/image
 mkdir -p $KIWI_BOXES
 
-if [ "$VARIANT" == "boxbuild" ]; then
-
 # build the image with locally installed kiwi and using boxbuild (qemu)
+if [ "$VARIANT" == "boxbuild" ]; then
 kiwi --profile $PROFILE \
 system boxbuild \
 --box ubuntu \
@@ -38,25 +37,11 @@ system boxbuild \
 --add-repo https://ftp.halifax.rwth-aachen.de/debian,apt-deb,$DEBIAN"_1",,,,,main,$DEBIAN,false \
 --add-repo https://ftp.halifax.rwth-aachen.de/debian,apt-deb,$DEBIAN"_2",,,,,contrib,$DEBIAN,false \
 --add-repo https://ftp.halifax.rwth-aachen.de/debian,apt-deb,$DEBIAN"_3",,,,,non-free,$DEBIAN,false
-
 exit
-# smlm
-# http://<smlm>/ks/dist/child/<repo>/<distribution>
---add-repo http://susemanager.suse/debian13-test-debian-13-pool-amd64/debian13-test,apt-deb,$DEBIAN"_1",,,,,main,$DEBIAN,false \
---add-repo http://susemanager.suse/debian13-test-debian-13-main-security-amd64/debian13-test,apt-deb,$DEBIAN"_2",,,,,contrib,$DEBIAN,false \
---add-repo http://susemanager.suse/debian13-test-debian-13-main-updates-amd64/debian13-test,apt-deb,$DEBIAN"_3",,,,,non-free,$DEBIAN,false \
---add-repo http://susemanager.suse/debian13-test-managertools-debian13-updates-amd64/debian13-test,apt-deb,$DEBIAN"_3",,,,,non-free,$DEBIAN,false
-
-#upstream:
---add-repo https://ftp.halifax.rwth-aachen.de/debian,apt-deb,$DEBIAN"_1",,,,,main,$DEBIAN,false \
---add-repo https://ftp.halifax.rwth-aachen.de/debian,apt-deb,$DEBIAN"_2",,,,,contrib,$DEBIAN,false \
---add-repo https://ftp.halifax.rwth-aachen.de/debian,apt-deb,$DEBIAN"_3",,,,,non-free,$DEBIAN,false
-
 fi
 
+# build the image with podman <- this is the future target as we do not install and maintain kiwi locally!
 if [ "$VARIANT" == "podman" ]; then
-
-# build the image with podman
 podman run --rm --privileged \
 -v /var/lib/ca-certificates:/var/lib/ca-certificates \
 -v /var/lib/Kiwi/repo:/var/lib/Kiwi/repo \
@@ -72,14 +57,35 @@ system boxbuild \
 --allow-existing-root \
 --ignore-repos \
 --ignore-repos-used-for-build \
+--ca-cert /usr/local/share/ca-certificates/RHN-ORG-TRUSTED-SSL-CERT.crt \
+--ca-target-distribution debian \
 --add-repo obs://Virtualization:Appliances:Builder/"Debian_"$DEBIAN_VER,apt-deb,kiwi,,,,,,,false \
 --add-repo obs://Virtualization:Appliances:Builder/"Debian_"$DEBIAN_VER"_x86_64",apt-deb,kiwi,,,,,,,false \
+--add-repo https://susemanager.weiss.ddnss.de/rhn/manager/download,apt-deb,$DEBIAN"_1",,,,,,debian13-test-debian-13-pool-amd64,false \
+--add-repo https://susemanager.weiss.ddnss.de/rhn/manager/download,apt-deb,$DEBIAN"_2",,,,,,debian13-test-debian-13-main-updates-amd64,false \
+--add-repo https://susemanager.weiss.ddnss.de/rhn/manager/download,apt-deb,$DEBIAN"_3",,,,,,debian13-test-debian-13-main-security-amd64,false \
+--add-repo https://susemanager.weiss.ddnss.de/rhn/manager/download,apt-deb,$DEBIAN"_4",,,,,,debian13-test-managertools-debian13-updates-amd64,false
+exit
+fi
+
+# smlm with authentication (stolen creds from another system) - need to find out how to auto-generate the required creds
+#
+--add-repo http://susemanager.weiss.ddnss.de:443/rhn/manager/download,apt-deb,$DEBIAN"_1",,,,,,debian13-test-debian-13-pool-amd64,false \
+--add-repo http://susemanager.weiss.ddnss.de:443/rhn/manager/download,apt-deb,$DEBIAN"_2",,,,,,debian13-test-debian-13-main-updates-amd64,false \
+--add-repo http://susemanager.weiss.ddnss.de:443/rhn/manager/download,apt-deb,$DEBIAN"_3",,,,,,debian13-test-debian-13-main-security-amd64,false \
+--add-repo http://susemanager.weiss.ddnss.de:443/rhn/manager/download,apt-deb,$DEBIAN"_4",,,,,,debian13-test-managertools-debian13-updates-amd64,false
+
+# smlm with distribution for non-auth access - does not work, yet - need to find out if that can work at all..
+# http://<smlm>/ks/dist/child/<repo>/<distribution>
+--add-repo http://susemanager.suse/debian13-test-debian-13-pool-amd64/debian13-test,apt-deb,$DEBIAN"_1",,,,,main,$DEBIAN,false \
+--add-repo http://susemanager.suse/debian13-test-debian-13-main-security-amd64/debian13-test,apt-deb,$DEBIAN"_2",,,,,contrib,$DEBIAN,false \
+--add-repo http://susemanager.suse/debian13-test-debian-13-main-updates-amd64/debian13-test,apt-deb,$DEBIAN"_3",,,,,non-free,$DEBIAN,false \
+--add-repo http://susemanager.suse/debian13-test-managertools-debian13-updates-amd64/debian13-test,apt-deb,$DEBIAN"_3",,,,,non-free,$DEBIAN,false
+
+#upstream:
 --add-repo https://ftp.halifax.rwth-aachen.de/debian,apt-deb,$DEBIAN"_1",,,,,main,$DEBIAN,false \
 --add-repo https://ftp.halifax.rwth-aachen.de/debian,apt-deb,$DEBIAN"_2",,,,,contrib,$DEBIAN,false \
 --add-repo https://ftp.halifax.rwth-aachen.de/debian,apt-deb,$DEBIAN"_3",,,,,non-free,$DEBIAN,false
-exit
-
-fi
 
 # for build on SMLM
 --add-repo file:/var/lib/Kiwi/repo,rpm-dir,common_repo,90,false,false \
